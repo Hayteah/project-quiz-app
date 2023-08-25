61f968b44ab393d16d08f9b8b00bd03cc76da2ae
Tunde <babatea4real@yahoo.com>
2023-08-25 10:39:56 +0200

    add audio source url changed to root directory

--- js/game.js
+++ js/game.js
@@ -600,11 +600,11 @@ class QuizGame {
       this.incrementScore(this.CORRECT_BONUS);
       selectedChoice.parentElement.classList.add(classToApply);
       let audio = new Audio();
-      audio.src = "/sound_correct.mp3";
+      audio.src = "./sound_correct.mp3";
       audio.play();
     } else {
       let audio = new Audio();
-      audio.src = "/sound_incorrect.mp3";
+      audio.src = "./sound_incorrect.mp3";
       audio.play();
       selectedChoice.parentElement.classList.add(classToApply);
     }
